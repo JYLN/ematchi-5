b941154d06a2ffcd88a6f6779ad7179fb6a05610
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-09-25 22:30:37 -0600

    :sparkles: Initial `Game` component

---
+++ src/lib/components/Game.svelte
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<main class="flex h-full flex-col items-center justify-center">
+	<!-- TODO: Add game info (pairs/moves/countdown) -->
+
+	<!-- TODO: Add game board -->
+
+	<!-- TODO: Add found pairs display -->
+</main>
