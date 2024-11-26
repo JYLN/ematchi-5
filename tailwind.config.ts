6449949202fbb59dfc0fa1a3ba43faf1d9ca55e6
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-26 11:18:26 -0700

    refactor: :recycle: prepare for Tailwind CSS v4 Beta migration
    - Relocate Poppins font import to layout
    - Remove utilty plugin from `tailwind.config.ts` due to errors with migration tool and new utility classes within v4 beta that cover the necessary styles the plugin addressed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
 
 export default {
 	darkMode: 'class',
@@ -15,48 +14,5 @@ export default {
 				height: 'height'
 			}
 		}
-	},
-
-	plugins: [
-		plugin(({ addUtilities, theme, e }) => {
-			const plainCssUtils = {
-				'.preserve-3d': {
-					'transform-style': 'preserve-3d',
-					'-webkit-transform-style': 'preserve-3d'
-				},
-				'.backface-hidden': {
-					'backface-visibility': 'hidden',
-					'-webkit-backface-visibility': 'hidden'
-				}
-			};
-
-			const rotateYValues = theme('rotate');
-			const rotateYUtils = {};
-
-			for (const key in rotateYValues) {
-				rotateYUtils[`.${e(`rotate-y-${key}`)}`] = {
-					transform: `rotateY(${rotateYValues[key]})`
-				};
-			}
-
-			const perspectiveValues = {
-				none: 'none',
-				xs: '100px',
-				sm: '250px',
-				md: '500px',
-				lg: '1000px',
-				xl: '2000px',
-				'2xl': '4000px'
-			};
-			const perspectiveUtils = {};
-
-			for (const key in perspectiveValues) {
-				perspectiveUtils[`.${e(`perspective-${key}`)}`] = {
-					perspective: perspectiveValues[key]
-				};
-			}
-
-			addUtilities({ ...plainCssUtils, ...rotateYUtils, ...perspectiveUtils });
-		})
-	]
+	}
 } as Config;
